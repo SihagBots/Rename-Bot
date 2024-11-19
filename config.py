bd8cdd2ccebe7006533ef9da02606ef3c129ea3d
SihagBots <sihagamit308@gmail.com>
2024-11-19 11:01:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,23 +3,23 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "29346781"))
+API_HASH = os.environ.get("API_HASH", "75fb004873db1864a09c71cd1307bfa8")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "5860332990"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQG_y90AAsCXEWwUCRXPY2EV8KRRVTvOXLFSsdYoHBnJ78VfcFsI1HMDY69aN_e8fOE3diERf2VWMeuHP1RIvK4z9UHLv3D6Rb9-aZqGGSBU-fUFPWnoZ-7R_47fgQmUDsorgRMBPGKltBbAVsnsq4rjrNett-9YRD7ORlzxh8nWlusRNW_durWmvGkkPN1QwgkIZPI1Z0ApqKWvyYTkdYdiGu2vsyLK87Iqs_IS9VEDedC7-ZpARVQk1CnfU-stOGlAAr6jXKIVqsoqdSPsKdJTd5Y_wdKhYj-ISWJhWWh65ATjR1UZoOLtV-gO1ijsUpWw5WlKErQ_yRTVFZIw-hLz4S8pkQAAAAFdTZW-AA")
 
 
 # Log & Force Channel Config
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002386658352"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://manojmanojs0014:amitsihag@cluster0.r2q9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "CodeXBots")
 
 
